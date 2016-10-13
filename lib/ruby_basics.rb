0aab6a5251e9849a0e47f1c15d00471c25f2f255
jeremye3180 <Jeremye3180@hstat.org>
2016-10-13 01:12:18 +0000

    finish tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(a,b)
+a/b    
 end
 
-def argue
-  
+def argue(a)
+  a
 end
 
-def greeting
-
+def greeting(greeting, name)
+puts "#{greeting}, #{name}"
 end
 
 def return_a_value
-    
+     "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping ="cheese")
+    "#{topping}"
 end
 
-def whisper
-    
+def whisper(word)
+    "#{word.downcase}"
 end
